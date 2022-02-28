5ce25c7692a0b0aa35946a4ebdb14ce080038774
eldermatheus <elder_matheus11@hotmail.com>
2022-02-28 19:14:11 -0300

    Aula 28-02-2022

---
+++ 06-Desafios-Iniciais/02-Quadrado-Cubo/Program.cs
@@ -0,0 +1,7 @@
+int n = int.Parse(Console.ReadLine());
+
+int inicio = 1;
+
+for (int i = 1; i <= n; i++) {                
+    Console.WriteLine($"{i} {i*i} {i*i*i}");
+}
---
+++ 06-Desafios-Iniciais/03-Soma-Pares-Consecutivos/Program.cs
@@ -0,0 +1,18 @@
+﻿int x = int.Parse(Console.ReadLine());
+
+while (x != 0) 
+{
+    int soma = 0;
+    for (int i = 0; i < 5; i++) 
+    {
+        if (x % 2 == 0) 
+        {
+            soma += x + (i + 2);
+        }
+        else             
+            soma += (x + 1) + (i + 2);
+        }
+
+        Console.WriteLine(soma);
+        x = int.Parse(Console.ReadLine());
+}
