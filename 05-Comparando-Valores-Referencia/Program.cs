a0560d717889b8c7f6241ce676b223ffd89abf57
eldermatheus <elder_matheus11@hotmail.com>
2022-02-28 19:14:29 -0300

    Aula 28-02-2022

---
+++ 05-Comparando-Valores-Referencia/Program.cs
@@ -0,0 +1,19 @@
+﻿Numero a = new Numero(2);
+Numero b = new Numero(2);
+
+// if (a == b): comparação de referência. Instâncias diferentes, mas objetos diferentes.
+// if (a.N == b.N): comparação de valor. Instâncias diferentes, mas valores de propriedades iguals.
+
+if (a == b) {
+    Console.WriteLine($"a e b são iguais");
+}
+else 
+    Console.WriteLine($"a e b são diferentes");
+
+class Numero {
+    public int N { get; set; }
+
+    public Numero(int numero){        
+        N = numero;
+    }    
+}
