d7f89a6b98d893b5c7769864f782c22c08e6cea7
eldermatheus <elder_matheus11@hotmail.com>
2022-03-07 18:08:13 -0300

    Aula 07-03-2022

---
+++ 15-Desafios-Básicos/Desafios-Iniciais/Desafios-Iniciais/Program.cs
@@ -0,0 +1,67 @@
+﻿int n, nota, quociente, resto;
+
+n = int.Parse(Console.ReadLine());
+Console.WriteLine(n);
+
+resto = n;
+
+while (resto > 0) {
+
+    nota = 100;
+    if (resto >= nota)
+    {
+        quociente = resto / nota;
+        Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+        resto %= nota;
+    }
+    else Console.WriteLine($"0 nota(s) de R$ {nota},00");
+
+    nota = 50;
+    if (resto >= nota)
+    {
+        quociente = resto / nota;
+        Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+        resto %= nota;
+    } else Console.WriteLine($"0 nota(s) de R$ {nota},00");
+
+    nota = 20;
+    if (resto >= nota)
+    {
+        quociente = resto / nota;
+        Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+        resto %= nota;
+    } else Console.WriteLine($"0 nota(s) de R$ {nota},00");
+
+    nota = 10;
+    if (resto >= nota)
+    {
+        quociente = resto / nota;
+        Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+        resto %= nota;
+    } else Console.WriteLine($"0 nota(s) de R$ {nota},00");
+
+    nota = 5;
+    if (resto >= nota)
+    {
+        quociente = resto / nota;
+        Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+        resto %= nota;    
+    } else Console.WriteLine($"0 nota(s) de R$ {nota},00");
+
+    nota = 2;
+    if (resto >= nota)
+    {
+        quociente = resto / nota;
+        Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+        resto %= nota;
+    } else Console.WriteLine($"0 nota(s) de R$ {nota},00");
+
+    nota = 1;
+    if (resto >= nota)
+    {
+        quociente = resto / nota;
+        Console.WriteLine($"{quociente} nota(s) de R$ {nota},00");
+        resto %= nota;
+    }
+    else Console.WriteLine($"0 nota(s) de R$ {nota},00");
+}
---
+++ 15-Desafios-Básicos/Desafios-Iniciais/Triangulo/Program.cs
@@ -0,0 +1,18 @@
+﻿double a, b, c;
+string[] valor = Console.ReadLine().Split();
+a = Convert.ToDouble(valor[0]);
+b = Convert.ToDouble(valor[1]);
+c = Convert.ToDouble(valor[2]);
+
+
+if (a + b > c && a + c > b && c + b > a)
+{
+    Console.WriteLine($"Perimetro = {a + b + c}");
+}
+else
+{
+    Console.WriteLine($"Area = {((a + b) * c) / 2}");
+}
+
+
+
