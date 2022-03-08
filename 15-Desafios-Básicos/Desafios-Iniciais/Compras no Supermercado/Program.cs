39f7e6e5b893824bcf77b1a986cd1931e605de71
eldermatheus <elder_matheus11@hotmail.com>
2022-03-07 23:14:49 -0300

    Aula 07-03-2022

--- 15-Desafios-Básicos/Desafios-Iniciais/Compras no Supermercado/Program.cs
+++ 15-Desafios-Básicos/Desafios-Iniciais/Compras no Supermercado/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var totalDeCasosDeTeste = int.Parse(Console.ReadLine());
+
+List<string> listaprodutos = new List<string>();
+
+for (int i = 0; i < totalDeCasosDeTeste; i++)
+{
+    string[] linha = Console.ReadLine().Split(' ');
+
+    foreach (var produto in linha)
+    {
+        if(!listaprodutos.Contains(produto))
+        listaprodutos.Add(produto);        
+    }
+
+    System.Console.WriteLine();
+    
+    listaprodutos.Sort();
+    Console.WriteLine(string.Join(" ", listaprodutos));
+    listaprodutos.Clear();
+}
+
--- 15-Desafios-Básicos/Desafios-Iniciais/Triangulo/Program.cs
+++ 15-Desafios-Básicos/Desafios-Iniciais/Triangulo/Program.cs
@@ -4,15 +4,39 @@ a = Convert.ToDouble(valor[0]);
 b = Convert.ToDouble(valor[1]);
 c = Convert.ToDouble(valor[2]);
 
-
 if (a + b > c && a + c > b && c + b > a)
 {
-    Console.WriteLine($"Perimetro = {a + b + c}");
+    double perimetro = a + b + c;
+    Console.WriteLine("Perimetro = {0:0.0}", perimetro);
 }
 else
 {
-    Console.WriteLine($"Area = {((a + b) * c) / 2}");
+    double area = (a + b) * c / 2;
+    Console.WriteLine("Area = {0:0.0}", area);
 }
 
+/*
+
+using System.Globalization;
+
+double a, b, c;
+string[] valor = Console.ReadLine().Split();
+a = Convert.ToDouble(valor[0].Replace(".", ","));
+b = Convert.ToDouble(valor[1].Replace(".", ","));
+c = Convert.ToDouble(valor[2].Replace(".", ","));
 
 
+if (a + b > c && a + c > b && c + b > a)
+{
+    double perimetro = a + b + c;
+    //Console.WriteLine(perimetro.ToString("F1", CultureInfo.InvariantCulture));
+    Console.WriteLine(perimetro);
+}
+
+else
+{
+    double area = (a + b) * c / 2;
+    //Console.WriteLine(area.ToString("F1", CultureInfo.InvariantCulture));
+    Console.WriteLine(area);
+}
+*/
